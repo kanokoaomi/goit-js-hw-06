3b8e5bc85f31af776c3eef3f7ac4ea491bf33a7e
kanoko_aomi <skoroglyadliza@gmail.com>
2024-07-03 00:02:23 +0200

    Update all tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,15 @@
-function getElementWidth(content, padding, border) {
-  const finalWidth =
-    Number.parseFloat(content) +
-    Number.parseFloat(padding) * 2 +
-    Number.parseFloat(border) * 2;
-  return finalWidth;
+function filterArray(numbers, value) {
+  const array = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      array.push(numbers[i]);
+    }
+  }
+  return array;
 }
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
